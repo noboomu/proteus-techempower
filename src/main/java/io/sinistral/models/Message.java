5e8507893381d76ca4fe3da8d306284edc04d85c
joshua bauer <josh@wurrly.com>
2017-06-16 13:58:51 -0700

    Fix for benchmark hostname :(

---
+++ src/main/java/io/sinistral/models/Message.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package io.sinistral.models;
+
+/**
+ * @author jbauer
+ *
+ */
+public final class Message
+{
+	public final String key;
+	public final String value;
+ 
+	public Message(String key, String value)
+	{
+		 
+		this.key = key;
+		this.value = value;
+	}
+	
+	
+}
