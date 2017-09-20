0abc99f2dd9b5a78154e6d7a2a9d3021ada33900
joshua bauer <josh@wurrly.com>
2017-09-20 13:57:51 -0700

    More tests.

--- src/main/java/io/sinistral/ExampleApplication.java
+++ src/main/java/io/sinistral/ExampleApplication.java
@@ -4,10 +4,14 @@ import static io.undertow.util.Headers.CONTENT_TYPE;
 
 import java.nio.ByteBuffer;
 
+import com.jsoniter.output.EncodingMode;
 import com.jsoniter.output.JsonStream;
 
 import io.sinistral.controllers.Benchmarks;
 import io.sinistral.models.Message;
+import io.sinistral.models.MessageEncoder;
+import io.sinistral.models.World;
+import io.sinistral.models.WorldEncoder;
 import io.sinistral.proteus.ProteusApplication;
 import io.sinistral.proteus.services.AssetsService;
 import io.sinistral.proteus.services.SwaggerService;
@@ -26,6 +30,25 @@ import io.undertow.server.handlers.SetHeaderHandler;
  */
 public class ExampleApplication extends ProteusApplication
 {
+	
+	private static final ByteBuffer MESSAGE_BUFFER;
+    private static final String MESSAGE = "Hello, World!";
+    
+    static {
+    	MESSAGE_BUFFER = ByteBuffer.allocateDirect(MESSAGE.length());
+    	try {
+    		MESSAGE_BUFFER.put(MESSAGE.getBytes("US-ASCII"));
+     } catch (Exception e) {
+    	 throw new RuntimeException(e);
+     	}
+    	MESSAGE_BUFFER.flip();
+    	
+    	JsonStream.setMode(EncodingMode.STATIC_MODE);
+    	 
+    	 
+    }
+    
+    
     public static void main( String[] args )
     {
         
@@ -45,7 +68,7 @@ public class ExampleApplication extends ProteusApplication
 		
 		PathHandler pathHandler = new PathHandler();
 		
-		pathHandler.addExactPath("/json2", new HttpHandler(){
+		pathHandler.addExactPath("/json", new HttpHandler(){
 			 @Override
 			  public void handleRequest(HttpServerExchange exchange) {
 			
@@ -57,6 +80,17 @@ public class ExampleApplication extends ProteusApplication
 			 }
 		});
 		
+		pathHandler.addExactPath("/plaintext", new HttpHandler(){
+			 @Override
+			  public void handleRequest(HttpServerExchange exchange) {
+			
+				 exchange.getResponseHeaders().put(CONTENT_TYPE, "text/plain");
+				    exchange.getResponseSender().send(MESSAGE_BUFFER.duplicate());
+			 }
+		});
+		
+		  
+		
 		 HttpHandler rootHandler = new SetHeaderHandler(pathHandler, "Server", "U-tow");
 		    Undertow.builder()
 		            .addHttpListener(8094, "0.0.0.0")
@@ -67,6 +101,20 @@ public class ExampleApplication extends ProteusApplication
 		            .setHandler(rootHandler)
 		            .build()
 		            .start();
+		    
+		    HttpHandler rootHandler2 = new SetHeaderHandler(pathHandler, "Server", "U-tow");
+		    Undertow.builder()
+		            .addHttpListener(8095, "0.0.0.0")
+		            .setBufferSize(16 * 1024)
+					.setIoThreads(8)
+					.setServerOption(UndertowOptions.ENABLE_HTTP2, false)
+					.setServerOption(UndertowOptions.ALWAYS_SET_DATE, true)
+					.setSocketOption(org.xnio.Options.BACKLOG, 100000)
+					.setServerOption(UndertowOptions.ALWAYS_SET_KEEP_ALIVE, false)
+					.setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, false) 
+					.setWorkerThreads(200)
+					 .build()
+			            .start();
     }
     
 }
